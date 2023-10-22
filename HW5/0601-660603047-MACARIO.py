e26de82663ab19c0b7c0e4a3da75db3e0f9cb232
davmacario <dav.macario@gmail.com>
2023-10-22 01:55:36 -0500

    Update hw5

--- HW5/0601-660603047-MACARIO.py
+++ HW5/0601-660603047-MACARIO.py
@@ -1,7 +1,7 @@
 import torch
-import torch.nn.functional as F
+from torch import nn
 import torchvision
-import torchvision.transforms as transforms
+from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -11,13 +11,21 @@ import shutil
 
 
 def splitDataset(
-    n_train: int, ds_path: str = os.path.join(os.path.dirname(__file__), "output")
+    n_train: int,
+    ds_path: str = os.path.join(os.path.dirname(__file__), "output"),
+    shuffle: bool = True,
 ):
     """
     splitDataset
     ---
     Given the path of the dataset, sort the images into two folders 'train/'
     and 'test/' containing the same number of elements per class.
+
+    The format of the folders is:
+
+    `train/<class>/<image>.png`
+
+    as stated in the PyTorch documentation.
 
     This function assumes that the file names are of the type:
 
@@ -27,6 +35,7 @@ def splitDataset(
     - n_train: number of training elements for each class
     - ds_path: path of the data set (defaults to 'output' folder in same
     directory as the script)
+    - shuffle: whether to shuffle the items before separation or not
 
     ### Output parameters
     - class_labels: list of class labels extracted from the file names
@@ -39,14 +48,14 @@ def splitDataset(
 
     # Create 'train' and 'test' folder (same dir as python file)
     # If either already exists, an exception is raised!
-    if not os.path.exists(os.path.join(__file__, "train")):
-        tr_path = os.path.join(__file__, "train")
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), "train")):
+        tr_path = os.path.join(os.path.dirname(__file__), "train")
         os.mkdir(tr_path)
     else:
         raise FileExistsError("'train' folder already exists")
 
-    if not os.path.exists(os.path.join(__file__, "test")):
-        te_path = os.path.join(__file__, "test")
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), "test")):
+        te_path = os.path.join(os.path.dirname(__file__), "test")
         os.mkdir(te_path)
     else:
         raise FileExistsError("'test' folder already exists")
@@ -54,32 +63,80 @@ def splitDataset(
     img_names = os.listdir(ds_path)  # File names (not abs. paths)
     class_labels = {}  # Key: class name, value: n. of items encountered so far
 
-    # TODO: maybe shuffle image names (but classes vector will have a different order)
+    # Shuffle items, if required
+    if shuffle:
+        random.shuffle(img_names)
 
     for fname in img_names:
         # Get class - if new one, append it to the vector of classes
         class_curr = str(fname.split("_")[0])
         if class_curr not in class_labels:
             class_labels[class_curr] = 0
+            os.mkdir(os.path.join(tr_path, class_curr))
+            os.mkdir(os.path.join(te_path, class_curr))
         else:
             class_labels[class_curr] += 1
 
         if class_labels[class_curr] <= n_train:
             # Place current image in training set
-            shutil.copy(os.path.join(ds_path, fname), os.path.join(tr_path, fname))
+            shutil.copy(
+                os.path.join(ds_path, fname), os.path.join(tr_path, class_curr, fname)
+            )
         else:
             # Place current image in test set
-            shutil.copy(os.path.join(ds_path, fname), os.path.join(te_path, fname))
+            shutil.copy(
+                os.path.join(ds_path, fname), os.path.join(te_path, class_curr, fname)
+            )
 
     return list(class_labels.keys()), tr_path, te_path
 
 
-def importDataset(train_path: str, test_path: str, classes: list):
+def importDataset(
+    train_path: str, classes: list = None, batch_size: int = 16, shuffle: bool = True
+) -> torch.utils.data.DataLoader:
     """
-    importDataset
+    importTraining
     ---
+    Import the training set, given the path.
+
+    ### Input parameters
+    - train_path: path of the trainin set folder
+    - classes: list of classes - if not given, they will be inferred
+    - batch_size: batch size in data loader
+    - shuffle: flag to select whether to shuffle the training elements
+    or not in data loader
+
+    ### Output parameters
+    - train_data_loader: `torch.utils.data.DataLoader` object containing the training set
     """
+    if not os.path.exists(train_path):
+        raise FileNotFoundError(f"The specified path ({train_path}) is invalid!")
+
+    img_names = os.listdir(train_path)
+    train_labels = []
+    for fname in img_names:
+        train_labels.append(str(fname.split("_")[0]))
+
+    transf = transforms.Compose([transforms.Resize((200, 200)), transforms.ToTensor()])
+    # TODO: maybe add transform.Normalize()
+
+    train_images = datasets.ImageFolder(train_path, transf)
+    train_data_loader = torch.utils.data.DataLoader(
+        train_images, batch_size=batch_size, shuffle=shuffle
+    )
+
+    return train_data_loader
 
 
 if __name__ == "__main__":
-    pass
+    dataset_path = os.path.join(os.path.dirname(__file__), "output")
+    train_path = os.path.join(os.path.dirname(__file__), "train")
+    n_training = 8000
+
+    try:
+        splitDataset(n_training, dataset_path)
+        print("Training and test sets generated!")
+    except FileExistsError:
+        print("Training and test set already split!")
+
+    dl_train = importDataset(train_path)
