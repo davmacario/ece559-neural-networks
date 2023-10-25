5ec6929afb3e5b0d711e30c7951836f774c6d5ab
davmacario <dav.macario@gmail.com>
2023-10-24 20:02:53 -0500

    Start inference module

---
+++ HW5/0603-660603047-MACARIO.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import torch
+from torch import nn
+from torchvision import transforms, datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import random
+import shutil
+from typing import Callable
